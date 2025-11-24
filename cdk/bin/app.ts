406eb67c7da819853689a5b812f652550e5f414c
David Amaku <amakudavid01@gmail.com>
2025-11-24 22:52:44 +0100

    cdk: update source-map-support dependency and clean up package-lock.json

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -1,4 +1,4 @@
-import 'source-map-support/register';
+import '@cspotcode/source-map-support/register'
 import * as cdk from 'aws-cdk-lib';
 import { VpcStack } from '../lib/vpc-stack';
 import { EksStack } from '../lib/eks-stack';
