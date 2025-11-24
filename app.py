d6eee6e0608658db1f8c41289358598ce52f3058
David Amaku <amakudavid01@gmail.com>
2025-11-24 14:39:16 +0100

    add: health endpoint for deployments

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def helloIndex():
-    return 'Hello, from MAX!'
+    return 'Hello, from Fincra!'
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "healthy"}), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
